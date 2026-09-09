ec36c893814e8f01bdbf72839e7d84bb19e28a83
root <root@ip-172-31-7-52.eu-north-1.compute.internal>
2026-09-09 11:07:45 +0000

    Fix CI build warnings

--- src/components/main-container.js
+++ src/components/main-container.js
@@ -29,7 +29,7 @@ export default class MainContainer extends Component {
       let data = result.data;
       let coins = [];
 
-      Object.keys(data).map((k,i) => {
+      Object.keys(data).forEach((k,i) => {
         if(data[k].last){
           coins.push(data[k]);
         }
