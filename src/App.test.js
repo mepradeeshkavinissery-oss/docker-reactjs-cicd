4affffffc03b64d0557c56b11a9061297cb70188
root <root@ip-172-31-7-52.eu-north-1.compute.internal>
2026-09-09 10:40:47 +0000

    Fix React test by mocking Axios

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+jest.mock('axios', () => ({
+  get: jest.fn(() => Promise.resolve({
+    data: {}
+  }))
+}));
+
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+
   ReactDOM.render(<App />, div);
+
   ReactDOM.unmountComponentAtNode(div);
 });
